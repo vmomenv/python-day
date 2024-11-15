df083e5f923bbd1606c63c531d227d2bdd3bbc2f
momen <vmomenv@gmail.com>
2024-11-15 11:18:03 +0800

    lambda函数增加与reduce函数的一起使用的案例。

--- lambda函数/main.py
+++ lambda函数/main.py
@@ -2,6 +2,7 @@
 #语法格式：lambda arguments:expression
 #lambda 是定义lambda函数；argument是参数列表；expression是表达式，返回计算结果
 
+
 #无参数lambda
 
 f=lambda :"hello world"
@@ -44,3 +45,16 @@ print("####lambda与filter函数使用####")
 #以下是输出偶数
 number=[1,2,3,4,5,6,7,8,9,10]
 print(list(filter(lambda x:x%2==0,number)))
+
+
+#############################################################################
+print("####lambda与reduce函数使用####")
+#以下是计算一个序列的累计乘积
+#使用reduce函数
+#reduce函数用法：对序列进行累计操作
+#reduce(function, iterable[, initializer]) function:可调用函数，对两个元素操作；iterable:迭代对象; initializer:初始值，如果有则作为累计操作的初始值
+#reduce会从iterable中取出元素，然后将前两个元素传递个function进行操作，然后结果和后一个进行操作，直到处理完所有元素。最终返回单一结果。
+
+from functools import reduce
+number=[1,2,3,4,5]
+print(reduce(lambda x,y:x*y,number))
